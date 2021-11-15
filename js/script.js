13b1ee9bb50cbd3e60920d81c959050b5f3a47bd
Rafael <khachikyan38@gmail.com>
2021-11-15 10:53:13 +0300

    tablet and desktop

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const sectionThree = document.querySelector(".giveaway__section--three");
 const sectionFour = document.querySelector(".giveaway__section--four");
 const sectionFive = document.querySelector(".giveaway__section--five");
 const progressBar = document.querySelector(".giveaway__progress-wrapper");
+const readMore = document.querySelector(".js-read-more");
 
 const [
   selectBtn,
@@ -36,15 +37,21 @@ const update = function () {
 };
 
 selectBtn.addEventListener("click", () => {
-  [product, overview].forEach((el) => el.classList.add("inactive"));
-  [progressBar, sectionOne].forEach((el) => el.classList.remove("inactive"));
+  [product, overview].forEach((el) => el.classList.add("inactive-section"));
+  [progressBar, sectionOne].forEach((el) =>
+    el.classList.remove("inactive-section")
+  );
 });
 
-const setActiveState = function (sectionInactive, sectionActive) {
+const setActiveState = function (
+  sectionInactive,
+  sectionActive,
+  className = "active-section"
+) {
   currentActive++;
-  sectionInactive.classList.add("inactive");
-  sectionActive.classList.remove("inactive");
-  sectionActive.classList.add("active--grid");
+  sectionInactive.classList.add("inactive-section");
+  sectionActive.classList.remove("inactive-section");
+  sectionActive.classList.add(className);
   update();
 };
 
@@ -52,7 +59,7 @@ sectionOneBtn.addEventListener("click", () =>
   setActiveState(sectionOne, sectionTwo)
 );
 sectionTwoBtn.addEventListener("click", () =>
-  setActiveState(sectionTwo, sectionThree)
+  setActiveState(sectionTwo, sectionThree, "grid-active")
 );
 sectionThreeBtn.addEventListener("click", () =>
   setActiveState(sectionThree, sectionFour)
@@ -64,4 +71,31 @@ sectionFourBtn.addEventListener("click", () =>
 sectionFiveBtn.addEventListener("click", () => {
   currentActive = 1;
   window.location.reload();
+});
+
+readMore.addEventListener("click", (e) => {
+  const html = `
+  <h4 class="giveaway__question">Key Ingredients</h4>
+  <p class="giveaway__answer">
+  Sunflower Seed Cake, Barley Extract and Cucumber Fruit Extract help strengthen skin’s barrier,
+  improve its resiliency, and balance and retain skin’s moisture levels.
+  Hyaluronic acid acts as a natural moisture magnet/humectant.
+  </p>
+
+<p class="giveaway__price">
+<s>us$25.89</s>
+<span class="giveaway__price-span">$0.00 FREE</span>
+</p>
+
+<button class="giveaway__btn js-btn" type="button">Select</button>
+
+<img
+src="./images/svg/amazon_prime_logo.png"
+alt=""
+aria-hidden="true"
+class="giveaway__amazon-prime"
+/>`;
+
+  e.target.insertAdjacentHTML("beforebegin", html);
+  e.target.remove();
 });
